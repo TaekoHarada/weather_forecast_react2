21ab0a4dddf92c7c0d53e1aa66e08ed3b87bcd12
TaekoHarada <taeko.harada@edu.sait.ca>
2023-12-25 17:14:06 -0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          Test Test
         </a>
       </header>
     </div>
