d7901432a7cb0326506d9fd419e436ab1de5cb60
TaekoHarada <taeko.harada@edu.sait.ca>
2023-12-25 17:47:21 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./styles/styles.css";
+import React from "react";
+import { Location } from "./components/Location";
 
 function App() {
   return (
@@ -18,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Location />
       <div>Weather forcast</div>
     </div>
   );
